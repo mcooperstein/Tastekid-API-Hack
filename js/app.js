b8ae554343abd4a3b9f77cb1e776b0cfb87c9010
Marc Cooperstein <mcooperstein@gmail.com>
2016-03-17 13:58:34 -0700

    final commit
    -formatted css
    -updated html
    -finalized js

--- js/app.js
+++ js/app.js
@@ -1,121 +1,74 @@
 // STEP 1 - get the input from the user
 $(document).ready(function () {
-    $('.search-movie').submit(function (event) {
-        //if the page refreshes when you submit the form use "preventDefault()" to force JavaScript to handle the form submission
-        event.preventDefault();
-        // zero out results if previous search has run
-        $('.results').html('');
-        // get the value of the tags the user submitted
-        var tags = $("#movieSearch").val();
-        //run the API search with the user input above
-        getTasteKidAPIResults(tags);
-        getOmdbAPIResults(tags);
-    });
-});
 
-// STEP 2 - using the input from the user (query) make the API call to get the JSON response
+    var getTasteKidAPIResults = function (searchTerm) {
 
-var getTasteKidAPIResults = function (searchTerm) {
+        var params = {
+            k: '212836-APITesti-7MEW060H',
+            type: 'movies',
+            q: searchTerm,
+            callback: 'foobar',
+            verbose: 1,
+            info: 1
+        };
+        var result = $.ajax({
+                /* update API end point */
+                url: "https://www.tastekid.com/api/similar",
+                data: params,
+                dataType: "jsonp",
+                /*set the call type GET / POST*/
+                type: "GET"
+            })
+            /* if the call is successful (status 200 OK) show results */
+            .done(function (result) { //show Results
+                /* if the results are meeningful, we can just console.log them */
+                if (result.Similar.Results.length === 0) {
+                    alert('No results found.');
+                } else {
+                    var output = "";
 
-    var params = {
-        k: '212836-APITesti-7MEW060H',
-        type: 'movies',
-        info: 0,
-        q: searchTerm,
-        callback: 'foobar'
+
+                    $.each(result.Similar.Results, function (key, value) {
+                        //concatenate the results inside the HTML variable
+                        console.log(value);
+                        output += '<li>';
+                        output += '<div class="movie-container">';
+                        output += '<div class="title-wrapper"><h3 class="movie-name">' + value.Name + '</h3></div>';
+                        output += '<br>'
+                        output += '<div class= "movie-details">'; //movie desctiption
+                        //Youtube clip URL yURL
+                        //Wikipedia URL wURL
+                        //Movie description
+                        output += '<p class="movie-teaser">' + "<span class='info-heading'><b>Information about the movie: </b></span>" + value.wTeaser + '</p>';
+                        output += '<br>'
+                        output += '<p class="youtube-clip"><a href="' + value.yUrl + '" target= _blank>' + "<span><b>Click here to watch the trailer:</b></span> " + value.yUrl + '</a></p>';
+                        output += '<br>'
+                        output += '<p class="wiki-info"><a href="' + value.wUrl + '" target= _blank>' + "<span><b>Click here for more info about the film:</b></span> " + value.wUrl + '</a></p>';
+                        output += '</div>';
+                        output += '</div>';
+                        output += '</li>';
+                    });
+
+                    $('.results ul').html(output);
+                }
+            });
     };
-    var result = $.ajax({
-            /* update API end point */
-            url: "https://www.tastekid.com/api/similar",
-            data: params,
-            dataType: "jsonp",
-            /*set the call type GET / POST*/
-            type: "GET"
-        })
-        /* if the call is successful (status 200 OK) show results */
-        .done(function (result) {
-            /* if the results are meeningful, we can just console.log them */
-            console.log(result);
-            showTasteKidResults(result);
-            //call here the function to display Tastekid API results
-        })
-        /* if the call is NOT successful show errors */
-        .fail(function (jqXHR, error, errorThrown) {
-            console.log(jqXHR);
-            console.log(error);
-            console.log(errorThrown);
-        });
-}
 
-// STEP 2 - using the input from the user (query) make the API call to get the JSON response
+    $('#search-button').on('click', function (e) {
+        e.preventDefault();
+        getTasteKidAPIResults($("#search-box").val());
+        $("#search-box").val('');
+        $('.results ul').show();
 
-var getOmdbAPIResults = function (searchTerm) {
+    });
+    $(document).on('keypress', function (e) {
+        //keyCode == 13 is the ENTER key
+        if (key.keyCode === 13) {
+            e.preventDefault();
+            getTasteKidAPIResults($("#search-box").val());
+            $("#search-box").val('');
+            $('.results ul').show();
+        }
+    });
 
-    var apiUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&type=movie&tomatoes=true";
-
-    var result = $.ajax({
-            /* update API end point */
-            url: apiUrl,
-            //data: params,
-            dataType: "json",
-            /*set the call type GET / POST*/
-            type: "GET"
-        })
-        /* if the call is successful (status 200 OK) show results */
-        .done(function (result) {
-            /* if the results are meeningful, we can just console.log them */
-            console.log(result);
-            showOmdbResults(result);
-            //call here the function to display Tastekid API results
-        })
-        /* if the call is NOT successful show errors */
-        .fail(function (jqXHR, error, errorThrown) {
-            console.log(jqXHR);
-            console.log(error);
-            console.log(errorThrown);
-        });
-}
-
-// STEP 3 (showTasteKidResults) - using the JSON response, populate the relevant part of your HTML with the variable inside the JSON
-
-var showTasteKidResults = function (movie) {
-    console.log(movie);
-    // clone our result template code
-    var result = $('.templates .movie').clone();
-
-    // Set the question properties in result
-    var movieElem = result.find('.movie-text a');
-    movieElem.attr('href', movie.link);
-    movieElem.text(movie.title);
-
-    return result;
-
-};
-
-
-// STEP 3 (showOmdbResults) - using the JSON response, populate the relevant part of your HTML with the variable inside the JSON
-
-var showOmdbResults = function (item) {
-    console.log(item);
-    var result = $('.templates .omdb').clone();
-    var imdb = result.find('.imdb-rating a')
-        .attr('href', item.link)
-        //.text(item.imdb.display_name);
-        //var image = "<img src='" + item.imdb.profile_image + "' alt='" + item.user.display_name + "'>";
-        //$(user).append(image);
-        //result.find('.imdb-rating').text(item.post_count);
-        //result.find('.rt-rating').text(item.score);
-
-    return result;
-
-};
-
-
-//EXTRA functions
-
-// takes error string and turns it into displayable DOM element
-var showError = function (error) {
-    var errorElem = $('.templates .error').clone();
-    var errorText = '<p>' + error + '</p>';
-    errorElem.append(errorText);
-};
+});
